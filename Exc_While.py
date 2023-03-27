08e1926f0c5767bd5b60837214ae2ec3a1273636
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2023-03-27 10:26:42 -0300

    Criei exercicio while

---
+++ Exc_While.py
@@ -0,0 +1,11 @@
+
+# Este programa pede um numero ao usuario, e imprime todos os pares, a partir de zero até o numero escolhido.
+# Criado em 27 - 03 por Rodrigo
+
+n = float(input('Escolha o valor de n: '))
+cont = 0
+
+while cont <= n:
+    print('Contador: ', cont)
+    cont += 2
+print('fim')
