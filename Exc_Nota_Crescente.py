0f03ef4755a9de95ab5f6ef128070f5c9a5de800
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2023-03-30 20:38:57 -0300

    Corrigi contagem regressiva, adicionei contagem

--- Exc_Nota_Crescente.py
+++ Exc_Nota_Crescente.py
@@ -2,18 +2,18 @@
 # Feito em: 24-03 por Rodrigo
 
 # Usando função sort
-print('-----------------')
-print('OBTENHA SUA NOTA')
-print('-----------------')
-print('\nInsira suas notas: \n')
-n1 = float(input('Nota 1: '))
-n2 = float(input('Nota 2: '))
-n3 = float(input('Nota 3: '))
-media = float(n1 + n2 + n3) / 3
-notas = [n1, n2, n3]
-notas.sort()
-print('\nSuas notas: ', notas)
-print(f'\nMédia: {media: .1f}')
+# print('-----------------')
+# print('OBTENHA SUA NOTA')
+# print('-----------------')
+# print('\nInsira suas notas: \n')
+# n1 = float(input('Nota 1: '))
+# n2 = float(input('Nota 2: '))
+# n3 = float(input('Nota 3: '))
+# media = float(n1 + n2 + n3) / 3
+# notas = [n1, n2, n3]
+# notas.sort()
+# print('\nSuas notas: ', notas)
+# print(f'\nMédia: {media: .1f}')
 
 # Usando if else
 print('-----------------')
@@ -25,15 +25,15 @@ n2 = float(input('Nota 2: '))
 n3 = float(input('Nota 3: '))
 media = float(n1 + n2 + n3) / 3
 # condicional
-if n1 < n2 and n1 < n3:
-    print("Suas notas: ", n1, n2, n3)
-elif n2 < n1 and n1 < n3:
-    print("Suas notas: ", n2, n1, n3)
-elif n3 < n2 and n2 < n1:
-    print("Suas notas: ", n3, n2, n1)
-elif n1 < n3 and n3 < n2:
-    print("Suas notas: ", n1, n3, n2 )
-elif n3 > n1 and n1 < n2:
-    print("Suas notas: ", n3, n1, n2)
+if n1 < n2 and n1 < n3 and n2 < n3:##
+    print("Suas notas: ", n1, n2, n3)## 
+elif n2 < n1 and n2 <n3 and n1 < n3:##
+    print("Suas notas: ", n2, n1, n3)##
+elif n1 < n3 and n1 < n2 and n3 < n2:##
+    print("Suas notas: ", n1, n3, n2 )##
+elif n3 < n1 and n3 < n2 and n1 < n2:
+    print("Suas notas: ", n3, n1, n2)##
+elif n2 < n1 and n2 < n3 and n3 < n1:##
+    print("Suas notas: ", n2, n3, n1) #--------
 else:
     print("Suas notas: ", n3, n2, n1)
---
+++ Exc_While_Contagem.py
@@ -0,0 +1,7 @@
+#recebe número 200
+#Faz uma contagem regressiva até 100, usando estrutura de repetição 
+
+n = 200
+while n >= 100:
+    n -= 1
+    print('N: ', n)
